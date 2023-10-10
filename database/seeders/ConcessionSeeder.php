6bc4823137f9a30c1f0bcfd270fe91b7e73da9fd
Emilien <emilien.cuny0312@gmail.com>
2023-10-10 10:15:10 +0200

    first commit

---
+++ database/seeders/ConcessionSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Concession;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+
+
+class ConcessionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Concession::create([
+            'name'=> 'La mine en or',
+            'siret'=> '1234678901234',
+            'phone'=> '06060606',
+        ]);
+    }
+}
---
+++ database/seeders/MineSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Mine;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MineSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Mine::create([
+            'name' => "TestMine",
+            'longitude' => 5.4754,
+            'latitude' => 7.45745,
+            'idConcession' => 1,
+        ]);
+    }
+}
